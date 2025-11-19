3092c945f55049055e865ea9178eb84b9579e8a0
Patrick Barabe <pbarabe@arizona.edu>
2025-11-19 14:15:28 -0700

    Update codebase per AZ Digital AZQS modules template
    Updates the project to adhere to coding and configuration standards.
    
    closes #11
    maintenance

--- src/Plugin/Block/GradDrupalCommonFooterBlock.php
+++ src/Plugin/Block/GradDrupalCommonFooterBlock.php
@@ -5,11 +5,11 @@ namespace Uazgraduatecollege\GradDrupalCommonFooter;
 use Drupal\Core\Block\BlockBase;
 
 /**
- * Provides a Grad Footer Block with common links
+ * Provides a Grad Footer Block with common links.
  *
  * @Block(
  *   id = "grad_common_footer_block",
- *   admin_label = @Translation("Grad Common Footer")
+ *   admin_label = @Translation("Grad Common Footer"),
  * )
  */
 class GradDrupalCommonFooterBlock extends BlockBase {
@@ -19,7 +19,7 @@ class GradDrupalCommonFooterBlock extends BlockBase {
    */
   public function build() {
     return [
-      '#theme' => 'grad_drupal_common_footer'
+      '#theme' => 'grad_drupal_common_footer',
     ];
   }
 
